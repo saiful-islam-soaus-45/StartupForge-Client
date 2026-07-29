6b4f6d5e9bbdfcfd8063ce1231d58f5c121bf1e1
saiful-islam-soaus-45 <soausahmedbd91@gmail.com>
2026-07-29 23:15:18 +0600

    google login done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -66,7 +66,14 @@ export const auth = betterAuth({
 
     }
   },
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ,
+        }, 
+    },
 
   plugins: [jwt()]
 
 });
+
